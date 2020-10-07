2b2dffc83fb1117dac096dd2767f65fdcfddea23
ichi-padal <kalinvlad@gmail.com>
2020-10-07 23:44:23 +0300

    preloader

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Preloader/>
+
+    <div>
+      <h1>PIDOR, GET WORK!</h1>
+    </div>
+    <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header";
+import Preloader from "@/components/Preloader";
 
 export default {
   name: 'App',
+  components: {
+    Header,
+    Preloader
+  }
 
 }
 </script>
 
 <style>
+
+body {
+  color: aliceblue;
+}
+
+body::-webkit-scrollbar {
+  width: 1em;
+}
+
+body::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: darkgrey;
+  outline: 1px solid slategrey;
+}
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h2>header</h2>
+    <Video/>
+  </header>
+</template>
+
+<script>
+import Video from "@/components/Video";
+
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  },
+  components: {
+    Video
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+
+</style>
